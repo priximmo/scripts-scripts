456cbc9b64431eb710c2bd074d581cdb530c6d66
Matthew Kehrer <matt91.mek@gmail.com>
2018-09-16 11:41:32 -0500

    chroot jail script working; creates functional, minimalist jail

--- chrt.sh
+++ chrt.sh
@@ -13,10 +13,10 @@ function uid_check {
 }
 
 function user_group_add {
-	if id -nu $2 1>/dev/null 2>1; then
+	if id -nu $2 1>/dev/null 2>/dev/null; then
 		declare -x user=`id -nu $2`
 		declare -x group`id -ng $3`
-	elif id -u $2 1>/dev/null 2>1; then
+	elif id -u $2 1>/dev/null 2>/dev/null; then
 		/bin/true
 	else
 		echo -e "Pass text for non created user/groups."
@@ -52,21 +52,33 @@ function make_dirs_in_chroot {
 	mkdir -p $CHRT_CHROOT_PREFIX/dev
 	mkdir -p $CHRT_CHROOT_PREFIX/etc 
 	mkdir -p $CHRT_CHROOT_PREFIX/lib
-	ln -s $CHRT_CHROOT_PREFIX/lib lib64 1>/dev/null 2>1
+	ln -s $CHRT_CHROOT_PREFIX/lib $CHRT_CHROOT_PREFIX/lib64 1>/dev/null 2>/dev/null
+	# /dev/*
+	mknod -m 666 $CHRT_CHROOT_PREFIX/dev/null c 1 3
+	mknod -m 666 $CHRT_CHROOT_PREFIX/dev/zero c 1 5
+	mknod -m 666 $CHRT_CHROOT_PREFIX/dev/tty c 5 0
+	mknod -m 666 $CHRT_CHROOT_PREFIX/dev/random c 1 8
+	mknod -m 666 $CHRT_CHROOT_PREFIX/dev/urandom c 1 9
+	chown root:tty $CHRT_CHROOT_PREFIX/dev/tty
 	# /usr
 	mkdir -p $CHRT_CHROOT_PREFIX/usr
 	mkdir -p $CHRT_CHROOT_PREFIX/usr/bin 
 	mkdir -p $CHRT_CHROOT_PREFIX/usr/lib 
-	ln -s $CHRT_CHROOT_PREFIX/lib lib64 1>/dev/null 2>1
-	ln -s $CHRT_CHROOT_PREFIX/usr/lib $CHRT_CHROOT_PREFIX/usr/li64 1>/dev/null 2>1 
+	ln -s $CHRT_CHROOT_PREFIX/lib $CHRT_CHROOT_PREFIX/lib64 1>/dev/null 2>/dev/null
+	ln -s $CHRT_CHROOT_PREFIX/usr/lib $CHRT_CHROOT_PREFIX/usr/lib64 1>/dev/null 2>/dev/null 
 	chmod -R 0775 $CHRT_CHROOT_PREFIX
 	chown -R root:root $CHRT_CHROOT_PREFIX 
 	ls -laZ $CHRT_CHROOT_PREFIX
 }
 
+function copy_configurations {
+	cp -fv --parents /etc/{group,prelink.cache,services,adjtime,shells,gshadow,shadow,hosts.deny,localtime,nsswitch.conf,nscd.conf,prelink.conf,protocols,hosts,passwd,ld.so.cache,ld.so.conf,resolv.conf,host.conf} $CHRT_CHROOT_PREFIX/etc/
+}
+
 function determine_binaries_deps {
 	make_dirs_in_chroot
-	if stat "$HOME"/".chrt_bins" 1>/dev/null 2>1; then
+	copy_configurations
+	if stat "$HOME"/".chrt_bins" 1>/dev/null 2>/dev/null; then
 		echo "using current binaries list"
 		export BINSF="${HOME}/.chrt_bins"
 		/bin/true
@@ -82,8 +94,12 @@ function determine_binaries_deps {
 }
 
 
-
 ## ## ENTRY
+
+if [[ $# -lt 3 ]]; then
+	echo -e "Usage:\n./chrt.sh <jail_path> <user_name> <group_name>\n"
+	/bin/false; exit 1
+fi
 
 echo -e "chroot jail creation scriptkit\n"
 echo FILES\ TO\ ALTER\ BEHAVIOR
@@ -95,6 +111,3 @@ uid_check
 user_group_add $1 $2 $3 # dir, usr, grp
 determine_binaries_deps
 
-
-
-#cp -fv /etc/{group,prelink.cache,services,adjtime,shells,gshadow,shadow,hosts.deny,localtime,nsswitch.conf,nscd.conf,prelink.conf,protocols,hosts,passwd,ld.so.cache,ld.so.conf,resolv.conf,host.conf}
